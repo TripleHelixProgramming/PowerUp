43acade2d6925b2582d8dd60651a325f8dbcafb2
robotics <robotics@172.24.16.28>
2018-01-20 14:00:35 -0500

    elevator subsystem created

---
+++ src/org/usfirst/frc/team2363/robot/subsystems/Elevator.java
@@ -0,0 +1,53 @@
+package org.usfirst.frc.team2363.robot.subsystems;
+
+import org.usfirst.frc.team2363.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.command.Subsystem;
+/**
+ *
+ */
+public class Elevator extends Subsystem {
+	
+	public enum Height {
+		
+		GROUND(0),
+		SWITCH(1),
+		SCALE(2),
+		RUNG(2);
+		
+		private final double height;
+		
+		private Height(double height) {
+			this.height = height;
+		}
+		
+		public double getHeight() {
+			return height;
+		}
+	}
+	
+	private TalonSRX leftMotor = new TalonSRX(RobotMap.LEFT_ELEVATOR_MOTOR);
+	private TalonSRX rightMotor = new TalonSRX(RobotMap.RIGHT_ELEVATOR_MOTOR);
+	private DigitalInput defaultPosition = new DigitalInput(RobotMap.DEFAULT_POSITION);
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+	public void elevate (double power) {
+		leftMotor.set(ControlMode.PercentOutput, power);
+	}
+	
+	public void goTo (Height height) {
+		leftMotor.set(ControlMode.Position, height.getHeight());
+	}
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
